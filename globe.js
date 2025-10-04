b29b190906bf296e5c7a50cdbbb7d213feb13b59
Rugman27 <89221756+RugMan27@users.noreply.github.com>
2025-10-04 15:44:27 -0400

    Added more points to globe.js

--- globe.js
+++ globe.js
@@ -4,9 +4,14 @@ const dayTexture = 'day_texture.png';
 const nightTexture = 'night_texture.png';
 
 const myData = [
-  { lat: 48.8575, lng: 2.3514, size: 0.8, color: 'red' }, //paris
-  { lat: 30.0444, lng: 31.2357, size: 0.8, color: 'blue' }, //cairo
-  { lat: 19.4326, lng: -99.1332, size: 0.8, color: 'green' } //mexico city
+    { lat: 48.8575, lng: 2.3514, size: 1, color: 'red' }, //paris
+    { lat: 30.0444, lng: 31.2357, size: 1, color: 'blue' }, //cairo
+    { lat: 19.4326, lng: -99.1332, size: 1, color: 'green' }, //mexico city
+    { lat: 35.6764, lng: 139.6500, size: 1, color: 'orange' }, //tokyo
+    { lat: -22.9068, lng: -43.1729, size: 1, color: 'yellow' }, //rio de janeiro
+    { lat: -42.70, lng: 172.217, size: 1, color: 'purple' }, //lake sumner forest
+    { lat: -71.9499, lng: 23.3470, size: 1, color: 'pink' }, //princess elisabeth
+    { lat: -48.526, lng: -123.236, size: 1, color: 'cyan' } //point nemo
 ];
 
 const myGlobe = Globe()(myDOMElement)
