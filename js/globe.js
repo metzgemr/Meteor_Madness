f6deb1833da76e2373acce3fc937a21bf9e59263
Rugman27 <89221756+RugMan27@users.noreply.github.com>
2025-10-04 21:53:11 -0400

    GOT IT WORKING
    Stuff was broken it fixed now

--- js/globe.js
+++ js/globe.js
@@ -9,13 +9,13 @@ let hoverD = null;
 
 const myData = [
     { lat: 48.8575, lng: 2.3514, size: 1, color: 'red', label: 'Paris', target: "/locations/paris.html" },
-    { lat: 30.0444, lng: 31.2357, size: 1, color: 'orange', label: 'Cairo', target: "/locations/cairo.html" },
-    { lat: 19.4326, lng: -99.1332, size: 1, color: 'yellow', label: 'Mexico City', target: "/locations/mexicocity.html" },
-    { lat: 35.6764, lng: 139.6500, size: 1, color: 'green', label: 'Tokyo', target: "/locations/tokyo.html" },
-    { lat: -22.9068, lng: -43.1729, size: 1, color: 'blue', label: 'Rio de Janeiro', target: "/locations/rio.html" },
-    { lat: -42.70, lng: 172.217, size: 1, color: 'cyan', label: 'Lake Sumner Forest', target: "/locations/sumner.html" },
-    { lat: -71.9499, lng: 23.3470, size: 1, color: 'purple', label: 'Princess Elisabeth', target: "/locations/elisabeth.html" },
-    { lat: -48.526, lng: -123.236, size: 1, color: 'pink', label: 'Point Nemo', target: "/locations/nemo.html" }
+    { lat: 30.0444, lng: 31.2357, size: 1, color: 'blue', label: 'Cairo', target: "/locations/cairo.html" },
+    { lat: 19.4326, lng: -99.1332, size: 1, color: 'green', label: 'Mexico City', target: "/locations/mexicocity.html" },
+    { lat: 35.6764, lng: 139.6500, size: 1, color: 'orange', label: 'Tokyo', target: "/locations/tokyo.html" },
+    { lat: -22.9068, lng: -43.1729, size: 1, color: 'yellow', label: 'Rio de Janeiro', target: "/locations/rio.html" },
+    { lat: -42.70, lng: 172.217, size: 1, color: 'purple', label: 'Lake Sumner Forest', target: "/locations/sumner.html" },
+    { lat: -71.9499, lng: 23.3470, size: 1, color: 'pink', label: 'Princess Elisabeth', target: "/locations/elisabeth.html" },
+    { lat: -48.526, lng: -123.236, size: 1, color: 'cyan', label: 'Point Nemo', target: "/locations/nemo.html" }
 ];
 
 // separate labels for display
